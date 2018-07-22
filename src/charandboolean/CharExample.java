1c8ad6666e12c5855e6f31dd2168adb94ecd2dc9
savaleks <savaleks@gmail.com>
2018-07-22 14:43:56 +0300

    unicode sample

---
+++ src/charandboolean/CharExample.java
@@ -0,0 +1,11 @@
+package charandboolean;
+
+public class CharExample {
+
+    public static void main(String[] args) {
+
+        char name = '\u00A9'; //unicode symbol
+
+        System.out.println(name);
+    }
+}
