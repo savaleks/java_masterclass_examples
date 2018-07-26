4c1b630c14e424ad7dd0b89d8508a5d0b3e0c4f6
savaleks <savaleks@gmail.com>
2018-07-26 12:18:22 +0300

    loops code examples from java challenge

---
+++ src/forloops/Challenge6.java
@@ -0,0 +1,57 @@
+package forloops;
+
+public class Challenge6 {
+    public static void main(String[] args) {
+//        System.out.println(hasSameLastDigit(41,22,71));
+//        System.out.println(hasSameLastDigit(23,32,42));
+//        System.out.println(hasSameLastDigit(9,99,999));
+
+//        System.out.println(getGreatestCommonDivisor(25,15));
+//        System.out.println(getGreatestCommonDivisor(12,30));
+//        System.out.println(getGreatestCommonDivisor(9,18));
+
+//        printFactors(32);
+//        printFactors(10);
+    }
+
+//    public static boolean hasSameLastDigit(int x, int y, int z){
+//        if ((x>=10&&x<=1000)&&(y>=10&&y<=1000)&&(z>=10&&z<=1000)){
+//            if ((x%10)==(y%10)){
+//                return true;
+//            } else if ((x%10)==(z%10)){
+//                return true;
+//            } else if ((y%10)==(z%10)){
+//                return true;
+//            } else {
+//                return false;
+//            }
+//        } return false;
+//    }
+
+//    public static int getGreatestCommonDivisor(int first, int second){
+//        if (first<10||second<10){
+//            return -1;}
+//            int result;
+//        if (first > second){
+//            result = second;
+//        } else {
+//            result = first;
+//        }
+//        while ((first%result != 0)||(second%result != 0)){
+//            result --;
+//        }
+//        return result;
+//        }
+//
+//    public static void printFactors(int number) {
+//        if(number < 1){
+//            System.out.println("Invalid Value");
+//        }
+//        int i;
+//        for (i = 1; i <= number; i++) {
+//            if (number % i == 0) {
+//                System.out.println(i);
+//            }
+//        }
+//    }
+}
