88cde497c8b7ee20a3c4f364c25070d61ecff285
savaleks <savaleks@gmail.com>
2018-09-04 10:44:30 +0300

    java quiz questions with answers

---
+++ src/test/quiz/Sample1.java
@@ -0,0 +1,38 @@
+package test.quiz;
+
+public class Sample1 {
+//        //1.question
+//        String s = "ABC";
+//        s.toLowerCase(); //not using that method;
+//        s+="def";
+//        System.out.println(s);
+//        //output: ABCdef;
+//
+//        System.out.println("------");
+
+//        //2.question
+//        int i = 0;
+//        while (1){ //should be boolean statement 'while(Boolean_expression)';
+//            if (i==4){
+//                break;;
+//            }
+//            ++i;
+//        }
+//        System.out.println("i = " + i);
+//        //compile error.int cannot be convert to boolean;
+//    }
+
+//        //3.question
+//        File obj = new File("/java/system");
+//        System.out.println(obj.getAbsolutePath());
+//        //output: \java\system
+//    }
+
+//        //4.question
+//        public static void main(String[] args) {
+//        }
+//        public void amethod(String[] arguments){}
+//        //output: empty line;
+
+        //5.question
+}
